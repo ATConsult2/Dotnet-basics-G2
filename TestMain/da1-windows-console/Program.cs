3b881d53c05ee0c2ce623dae2d529276a12af5df
Andres <a0z9@rambler.ru>
2022-10-10 16:59:50 +0300

    init main test, debugs

---
+++ TestMain/da1-windows-console/Program.cs
@@ -0,0 +1,27 @@
+﻿//using andestech.consulting2022.krasn.utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace andestech.consulting2022.krasn
+{
+    internal class Program
+    {
+        static void testDebug() {
+            WriteLine("Hi all from testDebug!!");
+        }
+
+        static void Main(string[] args)
+        {
+            WriteLine("Hi all!!");
+            WriteLine("Hi all 2!!");
+            testDebug();
+            utils.TestDebug.testDebug();
+            WriteLine("Hi all 3!!");
+
+        }
+    }
+}
---
+++ TestMain/da1-windows-console/TestDebug.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace andestech.consulting2022.krasn.utils
+{
+    internal static class TestDebug
+    {
+        public static void testDebug()
+        {
+            WriteLine("Hi all from TestDebug class!!");
+        }
+    }
+}
