1066c180d3fa92f57d0a1f2e963bcad015c2c667
Andres <a0z9@rambler.ru>
2022-10-17 14:32:11 +0300

    singleton

---
+++ TestApp4/ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace andestech.learning2022.krasn
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton s1 = Singleton.getInstance();
+            s1 = Singleton.getInstance();
+            s1 = Singleton.getInstance();
+        }
+    }
+}
