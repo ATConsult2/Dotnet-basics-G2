413ffb2535b3dfbe378c0ec6b30689f5b084fa4e
Andres <a0z9@rambler.ru>
2022-10-17 15:11:41 +0300

    Car test

--- TestApp4/ConsoleApp1/Vehicle.cs
+++ TestApp4/ConsoleApp1/Vehicle.cs
@@ -6,10 +6,10 @@ using static System.Console;
 
 namespace andestech.learning2022.krasn
 {
-    internal class Vehicle: object
+    internal class Vehicle : object
     {
-        public String Model { get; private set; }
-        public int Power { get; private set; } // params
+        protected String Model { get; private set; }
+        protected int Power { get; private set; } // params
 
         public Vehicle(string model, int power)
         {
@@ -22,18 +22,34 @@ namespace andestech.learning2022.krasn
         {
             return $"Vehicle: Model \"{Model}\", Power -> {Power} ";
         }
-
-
-
-
-
-
     }
 
+
+    enum EngineType { 
+     Bensine,
+     Diesel,
+     Gas
+    }
+    internal class Engine { 
+      public int Mass { get; private set; }
+      public int Power { get; private set; }
+      public EngineType EType { get; private set; }
+
+      public Engine(int mass, int power, EngineType eType)
+        {
+            Mass = mass;
+            Power = power;
+            EType = eType;
+        }   
+    }
     internal class Car : Vehicle
     {
-        public Car(string model, int power) : base(model, power)
+        public int Weels { get; private set; }
+        public Engine CarEngine { get; private set; }
+        public Car(string model, int power, Engine engine, int weels = 4) : base(model, power)
         {
+            CarEngine = engine;
+            Weels = weels;
             WriteLine("+++ ctor Car " + GetHashCode());
         }
     }
