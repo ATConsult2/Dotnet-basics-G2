8dbabf54743f27acce7d6da4da388bcd3cebfc5c
Andres <a0z9@rambler.ru>
2022-10-17 14:53:54 +0300

    Vehicle test

--- TestApp4/ConsoleApp1/Program.cs
+++ TestApp4/ConsoleApp1/Program.cs
@@ -14,6 +14,14 @@ namespace andestech.learning2022.krasn
             Singleton s1 = Singleton.getInstance();
             s1 = Singleton.getInstance();
             s1 = Singleton.getInstance();
+            //----------------------------
+            Vehicle v1 = new Vehicle(power: 122_000, model: "AA-1223");
+            WriteLine(v1);
+            //-----------------------------
+            WriteLine(new String('-', 55));
+            Car car = new Car("BMW", 200);
+            WriteLine(car);
+
         }
     }
 }
---
+++ TestApp4/ConsoleApp1/Vehicle.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using static System.Console;
+
+namespace andestech.learning2022.krasn
+{
+    internal class Vehicle: object
+    {
+        public String Model { get; private set; }
+        public int Power { get; private set; } // params
+
+        public Vehicle(string model, int power)
+        {
+            Model = model;
+            Power = power;
+            WriteLine("+++ ctor Vehicle " + GetHashCode());
+        }
+
+        public override string ToString()
+        {
+            return $"Vehicle: Model \"{Model}\", Power -> {Power} ";
+        }
+
+
+
+
+
+
+    }
+
+    internal class Car : Vehicle
+    {
+        public Car(string model, int power) : base(model, power)
+        {
+            WriteLine("+++ ctor Car " + GetHashCode());
+        }
+    }
+}
