4afdafbd9c0a14b219771a4ea92886ec3f9c54ce
Andres <a0z9@rambler.ru>
2022-10-19 14:16:38 +0300

    Operators

---
+++ TestApp5/ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+
+namespace andestech.learning2022.krasn
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Wire w1 = new Wire { Length = 10 };
+            Wire w2 = new Wire { Length = 2 };
+
+            Wire w3 = w2 + w1; //  operator  + !!
+            WriteLine(w3);
+            WriteLine(w2);
+            w3++;
+            w3++;
+            WriteLine(w3);
+            WriteLine("------------------------------");
+            Wire w4 = Wire.Join(ref w1, ref w2);
+            WriteLine(w4);
+            WriteLine(w2);
+
+        }
+    }
+}
---
+++ TestApp5/ConsoleApp1/Wire.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using static System.Console;
+
+
+namespace andestech.learning2022.krasn
+{
+    internal class Wire
+    {
+        public int Length {get;set;}
+
+        public override string ToString()
+        {
+            return "Wire: " + Length;
+        }
+
+        public static Wire operator +(Wire w1, Wire w2)
+        {
+            return new Wire { Length = w1.Length + w2.Length };
+        }
+
+        public static Wire operator ++(Wire w1)
+        {
+            w1.Length++;  return w1;
+        }
+
+        public static Wire operator --(Wire w1)
+        {
+            w1.Length--;  return w1;
+        }
+
+        public static Wire Join (ref Wire w1, ref Wire w2)
+        {
+            Wire w3 = new Wire { Length = w1.Length + w2.Length };
+            w1 = w2 = null;
+            return w3; 
+        }
+
+    }
+}
